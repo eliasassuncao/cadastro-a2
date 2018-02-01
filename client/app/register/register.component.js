cf93c913c09ba5c75f068daa23afc601de7b8e7b
Elias Assuncao <elias3748@gmail.com>
2018-02-01 16:43:30 -0200

    Cadastro

--- client/app/register/register.component.js
+++ client/app/register/register.component.js
@@ -5,17 +5,49 @@ var __decorate = (this && this.__decorate) || function (decorators, target, key,
     else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
     return c > 3 && r && Object.defineProperty(target, key, r), r;
 };
+var __metadata = (this && this.__metadata) || function (k, v) {
+    if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 var core_1 = require("@angular/core");
+var http_1 = require("@angular/http");
 var RegisterComponent = /** @class */ (function () {
-    function RegisterComponent() {
+    function RegisterComponent(http) {
+        this.client = {
+            nome: '',
+            genero: '',
+            cpf: '',
+            ddn: '',
+            telefone: ''
+        };
+        this.http = http;
     }
+    /*
+    register(event) {
+        event.preventDefault();
+        console.log(this.client);
+
+        let headers = new Headers();
+        headers.append('Content-Type', 'application/json');
+
+        this.http
+            .post('/v1/data', JSON.stringify(this.client) ,{ headers: headers} )
+            .subscribe( () => {
+                this.client = new Object;
+                console.log('Foto salva com sucesso');
+            });
+    } */
+    RegisterComponent.prototype.register = function (event) {
+        event.preventDefault();
+        console.log(this.client);
+    };
     RegisterComponent = __decorate([
         core_1.Component({
             moduleId: module.id,
             selector: 'register',
             templateUrl: './register.component.html'
-        })
+        }),
+        __metadata("design:paramtypes", [http_1.Http])
     ], RegisterComponent);
     return RegisterComponent;
 }());
--- client/app/register/register.component.ts
+++ client/app/register/register.component.ts
@@ -1,8 +1,48 @@
 import { Component } from '@angular/core';
+import { ListingComponent } from '../listing/listing.component';
+import { Http, Headers } from '@angular/http';
 
 @Component ({
     moduleId: module.id,
     selector: 'register',
     templateUrl: './register.component.html'
 })
-export class RegisterComponent {}
+export class RegisterComponent {
+
+    client: Object = {
+        nome: '',
+        genero: '',
+        cpf: '',
+        ddn: '',
+        telefone: ''
+    };
+    http: Http;
+
+    constructor(http: Http){
+        this.http = http;
+    }
+
+    /*
+    register(event) {
+        event.preventDefault();
+        console.log(this.client);
+
+        let headers = new Headers();
+        headers.append('Content-Type', 'application/json');
+
+        this.http
+            .post('/v1/data', JSON.stringify(this.client) ,{ headers: headers} )
+            .subscribe( () => {
+                this.client = new Object;
+                console.log('Foto salva com sucesso');
+            });
+    } */
+
+    register(event) {
+        event.preventDefault();
+        console.log(this.client);
+    }
+
+
+
+}
